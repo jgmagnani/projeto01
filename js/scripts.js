cfa3d2c77cfde4b81f2f3efe4711968db6f86370
jgmagnani <jogamabnu@gmail.com>
2022-10-17 15:34:01 -0300

    Projeto finalizado metade bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 let verPrato = "";
 let verBebida = "";
 let verSobremesa = "";
+let confirmaPedido = "";
+
+let compras = {};
 
 function escolherPrato(prato){
     
-    const verificaPrato = document.querySelector(".listaPratos .itemSelecionado");    
-   
+    const verificaPrato = document.querySelector(".listaPratos .itemSelecionado");   
+
     if (verificaPrato !== null){
         verificaPrato.classList.remove("itemSelecionado");              
-        verificaPrato.children[4].remove();        
+        verificaPrato.children[4].remove();
     }
-   
-    prato.classList.add("itemSelecionado");    
+       
+
+    prato.classList.add("itemSelecionado"); 
+    compras.nomePrato = document.querySelector(".listaPratos .itemSelecionado h1").innerHTML;
     prato.innerHTML += '<img class="check" src="imagens/Vector.png">';
     verPrato = prato;
+
+    
+
 }
 
 
@@ -23,9 +31,11 @@ function escolherBebida(bebida){
 
     if (verificaBebida !== null){
         verificaBebida.classList.remove("itemSelecionado");
+        verificaBebida.children[4].remove();
     }
 
     bebida.classList.add("itemSelecionado");
+    bebida.innerHTML += '<img class="check" src="imagens/Vector.png">';
     verBebida = bebida
     
 }
@@ -36,9 +46,11 @@ function escolherSobremesa(sobremesa){
 
     if (verificaSobremesa !== null){
         verificaSobremesa.classList.remove("itemSelecionado");
+        verificaSobremesa.children[4].remove();
     }
 
-    sobremesa.classList.add("itemSelecionado");    
+    sobremesa.classList.add("itemSelecionado");  
+    sobremesa.innerHTML += '<img class="check" src="imagens/Vector.png">';  
     verSobremesa = sobremesa
 }
 
@@ -46,6 +58,26 @@ setInterval(function(){
     const teste = document.querySelector(".verficaBotao")
     if( verPrato && verBebida && verSobremesa){
         teste.classList.add('itensSelecionados')
+        teste.innerHTML = "Fechar Pedido"
     }
 
 },150)
+
+
+function fecharPedido(){
+
+    const botaoConfirma = document.querySelector(".finalizar")
+    if( verPrato && verBebida && verSobremesa){
+        botaoConfirma.classList.add("displayNone");
+    }
+    confirmaPedido = botaoConfirma;
+}
+
+function cancelarPedido(){
+    confirmaPedido.classList.remove("displayNone")
+}
+
+function enviarPedido(){
+    //nome e valor da comida, bebida e sobremesa
+    alert(compras.nomePrato)
+}
