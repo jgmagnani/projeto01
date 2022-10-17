267f180fa9f132294a2117456e5b20b3e45704cf
jgmagnani <jogamabnu@gmail.com>
2022-10-16 23:40:54 -0300

    falta o botão finalizar

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
+function escolherPrato(prato){
+    
+    const verificaPrato = document.querySelector(".listaPratos .itemSelecionado");
+
+    if (verificaPrato !== null){
+        verificaPrato.classList.remove("itemSelecionado");
+    }
+
+    prato.classList.add("itemSelecionado");
+    
+    
+}
+
+
+function escolherBebida(bebida){
+    
+    const verificaBebida = document.querySelector(".listaBebidas .itemSelecionado");
+
+    if (verificaBebida !== null){
+        verificaBebida.classList.remove("itemSelecionado");
+    }
+
+    bebida.classList.add("itemSelecionado");
+    
+    
+}
+
+function escolherSobremesa(sobremesa){
+    
+    const verificaSobremesa = document.querySelector(".listaSobremesas .itemSelecionado");
+
+    if (verificaSobremesa !== null){
+        verificaSobremesa.classList.remove("itemSelecionado");
+    }
+
+    sobremesa.classList.add("itemSelecionado");
+    
+    
+}
+
 
